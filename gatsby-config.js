0761447592fda060717c9eaa9a0fa30e1316c9a7
Abu Bakar <iabubakardev@gmail.com>
2024-10-10 14:11:39 +0500

    new fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@iabubakardev',
   },
+  flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
